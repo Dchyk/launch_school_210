27d589338c6cf1cd80da2cd5e9ed1c02a99bade0
Brian Burns <alex.dzwonchyk@gmail.com>
2018-07-03 13:53:19 -0400

    Updated regex to properly ignore in-sentence punctuation

--- strings/longestSentence.js
+++ strings/longestSentence.js
@@ -1,5 +1,5 @@
 function longestSentence(text) {
-  var sentences = text.match(/[a-z]+\s?(,|\w|\s|-)+\b(.)/gi);
+  var sentences = text.match(/\b[^.!?]+[!?.]/gi);
 
   var sentenceData = sentences.map(function(sentence) { 
     return {
