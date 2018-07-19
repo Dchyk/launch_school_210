75bf8eb7367e72f88535645e30694b767c956ed2
Alex <hal@Alexs-Air.lan>
2018-07-19 10:22:51 -0400

    Make valid Luhn

--- exercises/interpretiveProblemSolving/luhnAlgorithm.js
+++ exercises/interpretiveProblemSolving/luhnAlgorithm.js
@@ -11,3 +11,27 @@ function validLuhn(num) {
     return a + b
   }) % 10 === 0;
 }
+
+// Given an invalid luhn number, write a function that can create a digit 
+// that when added to the input number, will create a valid luhn number.
+
+// Algorithm
+// - loop through the numbers 0 through 9, adding each to the front of the 
+//   input number and testing each for luhn validity.
+//   - when a valid luhn is found, return the current value of the added number.
+
+function makeValidLuhn(num) {
+  var checkDigit;
+  var testedLuhn;
+
+  if (validLuhn(num)) {
+    return num;
+  } else {
+    for (checkDigit = 0; checkDigit < 10; checkDigit += 1) {
+      testedLuhn = num + String(checkDigit);
+      if (validLuhn(testedLuhn)) {
+        return testedLuhn;
+      }
+    }
+  }
+}
